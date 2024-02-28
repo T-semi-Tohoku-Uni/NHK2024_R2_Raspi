e7e92222930949de83fcd92c6f5010d3b828a862
shibatakeigo <sagankeigo17@gmail.com>
2024-02-28 14:03:53 +0900

    Update library and Add template

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from NHK2024_Raspi_Library import MainController
+
+class R2Controller(MainController):
+    def main(self):
+        pass
+
+if __name__ == "__main__":
+    controller = R2Controller()
+    controller.main()
