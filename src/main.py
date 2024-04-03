14ed17655e349f7c23241ebd86de56f5b643f0c6
yuppy1005 <Y.Y.cricket@outlook.jp>
2024-04-03 17:09:21 +0900

    別ファイルに作ったので書き換え

--- src/main.py
+++ src/main.py
@@ -6,19 +6,8 @@ from typing import Dict, Callable
 from enum import Enum
 import can
 
-class CANList(Enum):
-    EMERGENCY=0x000
-    BATTERY_ERROR=0x001
-    
-    VACUUMFAN=0x100
-    ARM=0x101
-    ROBOT_VEL=0x106
-    
-    ROBOT_VEL_FB=0x204
-    WALL_DETECTION=0x205
-    LATERAL_SHIFT=0x206
-    ANGLE_DIFF=0x207
-    LINE_DETECT=0x208
+from behavior import Direction, Field, Behavior
+from hardware_module import CANList
     
 
 class ClientData:
@@ -33,7 +22,8 @@ class ClientData:
             self.btn_y = data["btn_y"]
         except KeyError as e:
             raise KeyError("Invalid key is included in the data: {e}")
-            
+
+
 class CANMessageLister(can.Listener):
     def __init__(self):
         super().__init__()
@@ -81,142 +71,6 @@ class CANMessageLister(can.Listener):
         #print(f"Received: {msg.__str__()}")
 
 
-class BaseAction:
-    def __init__(self):
-        pass
-
-    def move(self, v:[[float], [float], [float]]):
-        gain = [0.02, 0.02, 1]
-        TxBuffer = bytearray([127, 127, 127])
-        for i in range(3):
-            TxBuffer[i] = int(v[i] * gain[i] + 127)
-        
-        return TxBuffer
-
-    def arm(self, is_up):
-        TxBuffer = bytearray([0])
-        if is_up:
-            TxBuffer[0] = 0
-        elif is_up == False:
-            TxBuffer[0] = 1
-        else:
-            TxBuffer[0] = 0
-            print("Invalid value")
-        return TxBuffer
-
-    def vacuum_fan(self, is_on):
-        TxBuffer = bytearray([0])
-        if is_on:
-            TxBuffer[0] = 1
-        elif is_on == False:
-            TxBuffer[0] = 0 
-        else:
-            TxBuffer[0] = 0
-            print("Invalid value")
-        return TxBuffer
-
-class BehaviorList(Enum):
-    INITIALIZING = 0
-    INITIALIZIED = 4
-    START_READY = 8
-    ALIVE_AREA1 = 12
-    ALIVE_SLOPE12 = 16
-    ALIVE_AREA2_OUTER_WALL = 20
-    ALIVE_AREA2_WATER_WALL = 24
-    ALIVE_SLOPE23 = 28
-    ALIVE_AREA3_FIRST_ATTEMPT = 32
-    ALIVE_BALL_SEARCH_WIDE = 36
-    ALIVE_BALL_SEARCH_NARROW = 40
-    ALIVE_BALL_OBTAINIG = 44
-    ALIVE_MOVE_TO_SILO = 48
-    ALIVE_CHOOSE_SILO = 52
-    ALIVE_PUTIN = 56
-
-
-class Direction(Enum):
-    RIGHT = 0
-    FRONT = 1
-    LEFT = 2
-    BACK = 3  
-
-class Field(Enum):
-    BLUE = 0
-    RED = 1
-
-class Behavior:
-    def __init__(self, field = 0):
-        self.base_action = BaseAction()        
-        self.state_list = BehaviorList
-        self.state = self.state_list.INITIALIZING
-        self.field = field
-
-        '''
-        listner = CANMessageLister()
-        listner.init_write_can_bus_func(self.write_can_bus)
-        self.can_transmit_func = lister.write_can_bus
-        self.can_receive = None
-        '''
-        self.wall_sensor_state: list[bool] = [False, False, False, False, False, False, False, False]
-        self.posture_state: list[float] = {0, 0, 0, 0}
-
-    def change_state(self, state):
-        print('Change state from {} to {}'.format(self.state, state))
-        self.state = state
-
-    def update_sensor_state(self, state: Dict):
-        self.wall_sensor_state = state['wall_sensor']
-        #self.posture_state = state['posture']
-
-    def get_state(self):
-        return self.state
-
-    def move_along_wall(self, direction:int, move_direction:bool = True):
-        if direction > 3:
-            print("Invalid direction")
-            return False
-        max_speed = 500 
-        apploach_speed = 300
-        align_angle_speed = 1
-                
-        if direction == Direction.RIGHT.value:
-            if self.wall_sensor_state['Right front'] == False and self.wall_sensor_state['Right rear'] == False:
-                return self.base_action.move([apploach_speed, max_speed,  0])
-            elif self.wall_sensor_state['Right front'] == False and self.wall_sensor_state['Right rear'] == True:
-                return self.base_action.move([apploach_speed * 0.6, max_speed, -1 * align_angle_speed])
-            elif self.wall_sensor_state['Right front'] == True and self.wall_sensor_state['Right rear'] == False:
-                return self.base_action.move([apploach_speed * 0.6, max_speed, align_angle_speed])
-            elif self.wall_sensor_state['Right front'] == True and self.wall_sensor_state['Right rear'] == True:
-                return self.base_action.move([apploach_speed * 0.3, max_speed, 0])  
-            else:
-                print("Invalid wall sensor state")
-                return False
-        elif direction == Direction.LEFT.value:
-            pass
-        elif direction == Direction.FRONT.value:
-            pass
-        elif direction == Direction.BACK.value:
-            pass
-            
-    def action(self):
-        if self.state == self.state_list.INITIALIZING:
-            self.change_state(self.state_list.INITIALIZIED)
-            return
-        elif self.state == self.state_list.INITIALIZIED:
-            self.change_state(self.state_list.START_READY)
-            return
-        elif self.state == self.state_list.START_READY:
-            self.change_state(self.state_list.ALIVE_AREA1)
-            return
-        elif self.state == self.state_list.ALIVE_AREA1:
-            self.move_along_wall(Direction.RIGHT.value)     
-            return      
-        elif self.state == self.state_list.ALIVE_SLOPE12:
-            pass
-            
-        elif self.state == self.state_list.ALIVE_AREA2_OUTER_WALL:
-            pass
-
-    
 class R2Controller(MainController):
     def __init__(self):
         super().__init__("tsemiR2", 11111, is_udp=False)
