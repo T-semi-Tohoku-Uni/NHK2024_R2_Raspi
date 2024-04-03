a3d2b5f9b4e71cdf7cfec2c3f7d93db89f3fe5f6
yuppy1005 <Y.Y.cricket@outlook.jp>
2024-04-03 17:07:57 +0900

    refactor

--- src/behavior.py
+++ src/behavior.py
@@ -35,64 +35,87 @@ class Field(Enum):
     RED = 1
 
 
-class Behavior:
-    class BehaviorList(Enum):
-        INITIALIZING = 0
-        INITIALIZED = 4
-        START_READY = 8
-        ALIVE_AREA1 = 12
-        ALIVE_SLOPE12 = 16
-        ALIVE_AREA2_OUTER_WALL = 20
-        ALIVE_AREA2_WATER_WALL = 24
-        ALIVE_SLOPE23 = 28
-        ALIVE_AREA3_FIRST_ATTEMPT = 32
-        ALIVE_BALL_SEARCH_WIDE = 36
-        ALIVE_BALL_SEARCH_NARROW = 40
-        ALIVE_BALL_OBTAINIG = 44
-        ALIVE_MOVE_TO_SILO = 48
-        ALIVE_CHOOSE_SILO = 52
-        ALIVE_PUTIN = 56
+class BehaviorList(Enum):
+    INITIALIZING = 0
+    INITIALIZED = 4
+    START_READY = 8
+    ALIVE_AREA1 = 12
+    ALIVE_SLOPE12 = 16
+    ALIVE_AREA2_OUTER_WALL = 20
+    ALIVE_AREA2_WATER_WALL = 24
+    ALIVE_APPROACH_SLOPE23 = 27
+    ALIVE_SLOPE23 = 28
+    ALIVE_AREA3_FIRST_ATTEMPT = 32
+    ALIVE_BALL_SEARCH_WIDE = 36
+    ALIVE_BALL_SEARCH_NARROW = 40
+    ALIVE_BALL_OBTAINIG = 44
+    ALIVE_MOVE_TO_SILO = 48
+    ALIVE_CHOOSE_SILO = 52
+    ALIVE_PUTIN = 56
 
-    def __init__(self, field=0):
+
+class Behavior:
+    def __init__(self, field: Field, enable_log=True):
         self.base_action = BaseAction()
 
-        self.state = self.BehaviorList.INITIALIZING
+        self.state = BehaviorList.INITIALIZING
         self.field = field
 
-        self.wall_sensor_state: list[bool] = [False, False, False, False, False, False, False, False]
-        self.posture_state: list[float] = {0, 0, 0, 0}
+        self.wall_sensor_state: Dict = {
+            "Right rear": False,
+            "Right front": False,
+            "Front right": False,
+            "Front left": False,
+            "Left front": False,
+            "Left rear": False
+        }
+        self.posture_state: list[float] = [0, 0, 0, 0]
+        self.sensor_state: Dict = {}
 
-        self.log_system = LogSystem()
+        self.max_speed = 1000
+        self.position = [0, 0, 0]
 
-    def change_state(self, state):
+        self.can_messages = []
+
+        if enable_log:
+            self.log_system = LogSystem()
+        else:
+            self.log_system = None
+
+    def change_state(self, state: BehaviorList):
         print('Change state from {} to {}'.format(self.state, state))
-        self.log_system.write('Change state from {} to {}'.format(self.state, state))
+
+        if self.log_system is not None:
+            self.log_system.write('Change state from {} to {}'.format(self.state, state))
+            self.log_system.write('Sensor state: {}'.format(self.sensor_state))
         self.state = state
 
     def update_sensor_state(self, state: Dict):
+        self.sensor_state = state
         self.wall_sensor_state = state['wall_sensor']
         self.posture_state = state['posture']
 
     def get_state(self):
         return self.state
 
-    def move_along_wall(self, direction: int, move_direction: bool = True):
-        if direction > 3:
+    def move_along_wall(self, direction: Direction, move_direction: bool = True):
+        if direction not in Direction:
             print("Invalid direction")
             return False
         max_speed = 500
-        apploach_speed = 300
+        approach_speed = 400
         align_angle_speed = 1
 
-        if direction == Direction.RIGHT.value:
+        if direction == Direction.RIGHT:
+            
             if self.wall_sensor_state['Right front'] == False and self.wall_sensor_state['Right rear'] == False:
-                return self.base_action.move([apploach_speed, max_speed, 0])
+                return self.base_action.move([approach_speed, max_speed, 0])
             elif self.wall_sensor_state['Right front'] == False and self.wall_sensor_state['Right rear'] == True:
-                return self.base_action.move([apploach_speed * 0.6, max_speed, -1 * align_angle_speed])
+                return self.base_action.move([approach_speed * 0.1, max_speed, -1 * align_angle_speed])
             elif self.wall_sensor_state['Right front'] == True and self.wall_sensor_state['Right rear'] == False:
-                return self.base_action.move([apploach_speed * 0.6, max_speed, align_angle_speed])
+                return self.base_action.move([approach_speed * 0.1, max_speed, align_angle_speed])
             elif self.wall_sensor_state['Right front'] == True and self.wall_sensor_state['Right rear'] == True:
-                return self.base_action.move([apploach_speed * 0.3, max_speed, 0])
+                return self.base_action.move([approach_speed * 0.1, max_speed, 0])
             else:
                 print("Invalid wall sensor state")
                 return False
@@ -103,24 +126,104 @@ class Behavior:
         elif direction == Direction.BACK.value:
             pass
 
+    def calculate_position(self):
+        pass
+
     def action(self):
-        if self.state == self.BehaviorList.INITIALIZING:
-            self.change_state(self.BehaviorList.INITIALIZED)
-            return
-        elif self.state == self.BehaviorList.INITIALIZED:
-            self.change_state(self.BehaviorList.START_READY)
-            return
-        elif self.state == self.BehaviorList.START_READY:
-            self.change_state(self.BehaviorList.ALIVE_AREA1)
-            return
-        elif self.state == self.BehaviorList.ALIVE_AREA1:
-            self.move_along_wall(Direction.RIGHT.value)
+        self.can_messages.clear()
+        if self.state == BehaviorList.INITIALIZING:
+            self.change_state(BehaviorList.INITIALIZED)
+            return self.can_messages
+
+        elif self.state == BehaviorList.INITIALIZED:
+            self.change_state(BehaviorList.START_READY)
+            return self.can_messages
+
+        #スタート準備OK
+        elif self.state == BehaviorList.START_READY:
+            self.change_state(BehaviorList.ALIVE_AREA1)
+            return self.can_messages
+
+        #エリア１の壁に沿って進む
+        elif self.state == BehaviorList.ALIVE_AREA1:
+            if self.field == Field.BLUE:
+                self.can_messages.append(self.move_along_wall(Direction.RIGHT))
+            elif self.field == Field.RED:
+                self.can_messages.append(self.move_along_wall(Direction.LEFT))
             if self.posture_state[0] < 0:
                 self.change_state(self.BehaviorList.ALIVE_SLOPE12)
-            return
+            return self.can_messages
+
+        #スロープのぼる
+        elif self.state == BehaviorList.ALIVE_SLOPE12:
+            if self.field == Field.BLUE:
+                self.can_messages.append(self.move_along_wall(Direction.RIGHT))
+            elif self.field == Field.RED:
+                self.can_messages.append(self.move_along_wall(Direction.LEFT))
+
+            #坂から抜けだしたら次の状態へ
+            if self.posture_state[0] > 0:
+                self.can_messages.append(self.change_state(self.BehaviorList.ALIVE_AREA2_OUTER_WALL))
+            return self.can_messages
+
+        #エリア２のスロープから水ゾーンの壁への遷移
+        elif self.state == BehaviorList.ALIVE_AREA2_OUTER_WALL:
+            radius = 2500
+            center = 2500
+            sign = 1
+            if self.field == Field.BLUE:
+                sign = 1
+            elif self.field == Field.RED:
+                sign = -1
+            v = [0, self.max_speed, sign * self.max_speed / radius]
+
+            #機体が横向きになったら次の状態へ
+            if self.posture_state[0] > center:
+                self.change_state(BehaviorList.ALIVE_AREA2_WATER_WALL)
+            return self.can_messages.append(self.base_action.move(v))
+
+        #水ゾーンの壁に伝って進む
+        elif self.state == BehaviorList.ALIVE_AREA2_WATER_WALL:
+            if self.field == Field.BLUE:
+                self.can_messages.append(self.move_along_wall(Direction.RIGHT))
+            elif self.field == Field.RED:
+                self.can_messages.append(self.move_along_wall(Direction.LEFT))
+
+            #右前のセンサが反応しなくなったら
+            if not self.wall_sensor_state[1]:
+                self.can_messages.clear()
+                #右後ろのセンサが反応しなくなるまでゆっくり進む
+                if self.wall_sensor_state[0]:
+                    self.can_messages.append(self.base_action.move([0, 300, 0]))
+
+                elif not self.wall_sensor_state[0]:
+                    self.change_state(BehaviorList.ALIVE_APPROACH_SLOPE23)
+
+            return self.can_messages
+
+        #スロープに近づく
+        elif self.state == BehaviorList.ALIVE_APPROACH_SLOPE23:
+            self.can_messages.append(self.base_action.move([0, 300, 0]))
 
 
-def __main__():
-    while True:
-        behavior = Behavior()
-        behavior.action()
+            #右後ろのセンサが反応しなくなったら
+            if not self.wall_sensor_state[0]:
+                self.change_state(BehaviorList.ALIVE_SLOPE23)
+            return self.can_messages
+
+
+if __name__ == '__main__':
+    behavior = Behavior(Field.BLUE, True)
+    try:
+        while True:
+            sensor_state = {
+                'wall_sensor': {"Right rear": False, "Right front": False, "Front right": False, "Front left": False, "Left front": False, "Left rear": False},
+                'posture': [0, 0, 0, 0]
+            }
+            behavior.update_sensor_state(sensor_state)
+
+            behavior.action()
+            behavior.get_state()
+
+    except KeyboardInterrupt:
+        pass
